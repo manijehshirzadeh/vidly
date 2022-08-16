3174c06dd8e9b05f3699de6c05016aa506d2d9c0
manijehshirzadeh <manijeh.shirzadeh63@gmail.com>
2022-08-16 18:27:38 +1000

    Added routes in the app bar. Created some place holder components for the routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
         <Movies />
-      </main>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
